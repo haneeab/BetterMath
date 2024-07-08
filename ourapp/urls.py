9df375f9fd437768a12bd696a3bad3b659b6c76b
RaghadAbdalhlem <117685384+RaghadAbdalhlem@users.noreply.github.com>
2024-07-08 13:31:12 +0300

    I changed in models.py urls.py forms.py and views.py

---
+++ ourapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import register_student, register_teacher
+from ourapp import views
+
+urlpatterns = [
+
+    path('register_student.html', views.register_student, name='register_student'),
+    # path('register_teacher.html', views.register_teacher, name='register_teacher'),
+
+]
