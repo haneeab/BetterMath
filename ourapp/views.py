3f5b13a91e2492084ce6326e137bc2d90fadf891
einasns <einas.mansor@gmail.com>
2024-07-08 12:23:59 +0300

    homepage

--- ourapp/views.py
+++ ourapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'HomePage.html')
+
+
+
+
+
+
+
+
+def contact(request):
+    return HttpResponse('contact Page')
